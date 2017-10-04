c6d5152e4d0c5835e40c92442b3e7c39e3b1968d
P. Dayvson <pdayvson@hotmail.com>
2017-10-04 11:03:31 -0300

    Adicionando consign

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var app = require("./config/server")
 
-var rotaNoticias = require("./app/routes/noticias")(app)
+// var rotaNoticias = require("./app/routes/noticias")(app)
 
-var rotaHome = require("./app/routes/home")(app)
+// var rotaHome = require("./app/routes/home")(app)
 
 app.listen(3000, function () {
-    console.log("Servidor ON")
+  console.log("Servidor ON")
 })
